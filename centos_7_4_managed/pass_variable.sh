85d8746cc0527fb67302a6b2343468e73b7cbaf1
ocb <abhay.srivastava@orange.com>
2018-07-31 04:20:16 +0000

    Revert "Revert "added file new revert"
    This reverts commit 687ba101a48b275327fb4a356373db3af5d1fe86.

---
+++ centos_7_4_managed/pass_variable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -xe
+echo "instance_name: "$instance_name > /opt/abhay/project/centos_7_4_managed/var_stuff.yml
+echo "image_name: "$image_name >> /opt/abhay/project/centos_7_4_managed/var_stuff.yml
